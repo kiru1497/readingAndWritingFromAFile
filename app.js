144a2fc2be1744fc4a3dab88f376320feb12ec07
Kiran R <kiran.r1497@gmail.com>
2025-12-19 12:33:06 +0530

    third commit

--- app.js
+++ app.js
@@ -1,44 +1,8 @@
 const http = require("http");
-const fs = require("fs"); 
 
-const server = http.createServer((req, res) => {
-  
-  if (req.url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.end(`
-      <form action="/message" method="POST">
-        <input type="text" name="username" placeholder="Enter name" required />
-        <br><br>
-        <button type="submit">Add</button>
-      </form>
-    `);
-  }
-  else{
-    if(req.url === '/message'){
-      res.setHeader("Content-Type", "text/html")
-      let body = []; 
-      req.on('data', (chunks)=> {
-        body.push(chunks);
-      })
+const routes = require("./routes")
 
-      req.on('end',()=>{
-        let buffer=Buffer.concat(body);
-      
-        let formData = buffer.toString(); 
-        console.log(formData); 
-
-        const formValues = formData.split("=")[1]; 
-
-        fs.writeFile('formValue.txt',formValues, (err)=>{
-
-          res.statusCode = 302; 
-          res.setHeader("location","/");
-          res.end();
-        })
-      })
-    }
-  }
-});
+const server = http.createServer(routes);
 
 server.listen(3000, () => {
   console.log("Server is running");
---
+++ routes.js
@@ -0,0 +1,41 @@
+const fs = require("fs"); 
+
+const requestHandler = (req,res)=> {
+    if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.end(`
+      <form action="/message" method="POST">
+        <input type="text" name="username" placeholder="Enter name" required />
+        <br><br>
+        <button type="submit">Add</button>
+      </form>
+    `);
+  }
+  else{
+    if(req.url === '/message'){
+      res.setHeader("Content-Type", "text/html")
+      let body = []; 
+      req.on('data', (chunks)=> {
+        body.push(chunks);
+      })
+
+      req.on('end',()=>{
+        let buffer=Buffer.concat(body);
+      
+        let formData = buffer.toString(); 
+        console.log(formData); 
+
+        const formValues = formData.split("=")[1]; 
+
+        fs.writeFile('formValue.txt',formValues, (err)=>{
+
+          res.statusCode = 302; 
+          res.setHeader("location","/");
+          res.end();
+        })
+      })
+    }
+  }
+}
+
+module.exports = requestHandler;
